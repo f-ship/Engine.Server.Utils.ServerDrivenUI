c7485cb8fc263e129d6dd6da036876b6f2d61c24
Sam Taiwo <staiwo@onlydates.fan>
2024-12-24 21:38:42 +0000

    Initial Commit

---
+++ src/main/kotlin/ship/f/engine/server/utils/serverdrivenui/ServerDrivenUI.kt
@@ -0,0 +1,21 @@
+package ship.f.engine.server.utils.serverdrivenui
+
+import io.ktor.server.application.Application
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+import io.ktor.server.netty.Netty
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    routing {
+        get("/") {
+            call.respondText("Ktor: Sample Server")
+        }
+    }
+}
